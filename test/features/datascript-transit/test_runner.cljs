b91002aaddf3381655209054cea8313af6d5f607
Gabriel Horner <gabriel@logseq.com>
2023-01-06 17:21:56 -0500

    Revert "Move destructuring out of defmethod to avoid nbb bug for now"
    This reverts commit 4256153979c0cb2ef54b92e9bc78df9c19e8df9c.

--- test/features/datascript-transit/test_runner.cljs
+++ test/features/datascript-transit/test_runner.cljs
@@ -3,11 +3,10 @@
             [nbb.core :as nbb]
             [datascript.test.transit]))
 
-(defmethod t/report [::t/default :end-run-tests] [m]
-  (let [{:keys [error fail]} m]
-    (if (pos? (+ error fail))
-      (js/process.exit 1)
-      (js/process.exit 0))))
+(defmethod t/report [::t/default :end-run-tests] [{:keys [error fail]}]
+  (if (pos? (+ error fail))
+    (js/process.exit 1)
+    (js/process.exit 0)))
 
 (defn init []
   (t/run-tests 'datascript.test.transit))
